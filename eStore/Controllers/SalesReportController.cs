21162b61650cc59554e0c0252dfcb305267c1cb0
DangQuangTien <dangquangtien16102002@gmail.com>
2025-07-12 14:24:32 +0700

    Add API controllers and enable controller routing

---
+++ eStore/Controllers/SalesReportController.cs
@@ -0,0 +1,20 @@
+using BusinessObject.DTOs;
+using DataAccess.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eStore.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SalesReportController : ControllerBase
+{
+    private readonly IOrderService _service;
+
+    public SalesReportController(IOrderService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<SalesReportDto>> Get(DateTime startDate, DateTime endDate) => Ok(_service.GetSalesReport(startDate, endDate));
+}
--- eStore/Program.cs
+++ eStore/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -41,6 +43,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.MapControllers();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
